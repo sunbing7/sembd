74165cbe37bc0e469a88471ac447435686c55c79
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-08-13 20:50:06 +0800

    remove unused api

--- fashion/source/fmnist_analysis2.py
+++ fashion/source/fmnist_analysis2.py
@@ -32,8 +32,6 @@ MODEL_DIR = '../models'  # model directory
 MODEL_FILENAME = 'fmnist_semantic_6_attack.h5'  # model file
 
 RESULT_DIR = '../results2'  # directory for storing results
-# image filename template for visualization results
-IMG_FILENAME_TEMPLATE = 'fmnist_visualize_%s_label_%d.png'
 
 BATCH_SIZE = 32  # batch size used for optimization
 NB_SAMPLE = 1000  # number of samples in each mini batch
@@ -48,11 +46,8 @@ INPUT_SHAPE = (IMG_ROWS, IMG_COLS, IMG_COLOR)
 NUM_CLASSES = 10  # total number of classes in the model
 Y_TARGET = 2  # (optional) infected target label, used for prioritizing label scanning
 
-INTENSITY_RANGE = 'raw'  # preprocessing method for the task, GTSRB uses raw pixel intensities
-
 # parameters for optimization
 BATCH_SIZE = 32  # batch size used for optimization
-TEST_BATCH_SIZE = 5
 
 AE_TRAIN = [72,206,235,314,361,586,1684,1978,3454,3585,3657,4290,4360,4451,4615,4892,5227,5425,5472,5528,5644,5779,6306,6377,6382,6741,6760,6860,7231,7255,7525,7603,7743,7928,8251,8410,8567,8933,8948,9042,9419,9608,10511,10888,11063,11164,11287,11544,11684,11698,11750,11990,12097,12361,12427,12484,12503,12591,12915,12988,13059,13165,13687,14327,14750,14800,14849,14990,15019,15207,15236,15299,15722,15734,15778,15834,16324,16391,16546,16897,17018,17611,17690,17749,18158,18404,18470,18583,18872,18924,19011,19153,19193,19702,19775,19878,20004,20308,20613,20745,20842,21271,21365,21682,21768,21967,22208,22582,22586,22721,23574,23610,23725,23767,23823,24435,24457,24574,24723,24767,24772,24795,25039,25559,26119,26202,26323,26587,27269,27516,27650,27895,27962,28162,28409,28691,29041,29373,29893,30227,30229,30244,30537,31125,31224,31240,31263,31285,31321,31325,31665,31843,32369,32742,32802,33018,33093,33118,33505,33902,34001,34523,34535,34558,34604,34705,34846,34934,35087,35514,35733,36265,36943,37025,37040,37175,37690,37715,38035,38183,38387,38465,38532,38616,38647,38730,38845,39543,39698,39832,40358,40622,40713,40739,40846,41018,41517,41647,41823,41847,42144,42481,42690,43133,43210,43531,43634,43980,44073,44127,44413,44529,44783,44951,45058,45249,45267,45302,45416,45617,45736,45983,46005,47123,47557,47660,48269,48513,48524,49089,49117,49148,49279,49311,49780,50581,50586,50634,50682,50927,51302,51610,51622,51789,51799,51848,52014,52148,52157,52256,52259,52375,52466,52989,53016,53035,53182,53369,53485,53610,53835,54218,54614,54676,54807,55579,56672,57123,57634,58088,58133,58322,59037,59061,59253,59712,59750]
 AE_TST = [1939,7,390,586,725,726,761,947,1071,1352,1754,1944,2010,2417,2459,2933,3129,3545,3661,3905,4152,4606,5169,6026,6392,6517,6531,6540,6648,7024,7064,7444,8082,8946,8961,8974,8984,9069,9097,9206,9513,9893]
@@ -65,114 +60,6 @@ TARGET_LABEL = [0,0,0,0,1,0,0,0,0,0]
 ##############################
 #      END PARAMETERS        #
 ##############################
-
-def load_dataset():
-    # the data, split between train and test sets
-    (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.fashion_mnist.load_data()
-
-    # Scale images to the [0, 1] range
-    x_train = x_train.astype("float32") / 255
-    x_test = x_test.astype("float32") / 255
-    # Make sure images have shape (28, 28, 1)
-    x_train = np.expand_dims(x_train, -1)
-    x_test = np.expand_dims(x_test, -1)
-    #print("x_train shape:", x_train.shape)
-    #print(x_train.shape[0], "train samples")
-    #print(x_test.shape[0], "test samples")
-
-    # convert class vectors to binary class matrices
-    y_train = tensorflow.keras.utils.to_categorical(y_train, NUM_CLASSES)
-    y_test = tensorflow.keras.utils.to_categorical(y_test, NUM_CLASSES)
-
-    return x_train, y_train, x_test, y_test
-
-def load_dataset_class(cur_class=0):
-    # the data, split between train and test sets
-    (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.fashion_mnist.load_data()
-
-    # Scale images to the [0, 1] range
-    x_train = x_train.astype("float32") / 255
-    x_test = x_test.astype("float32") / 255
-    # Make sure images have shape (28, 28, 1)
-    x_train = np.expand_dims(x_train, -1)
-    x_test = np.expand_dims(x_test, -1)
-    print("x_train shape:", x_train.shape)
-    print(x_train.shape[0], "train samples")
-    print(x_test.shape[0], "test samples")
-
-    # convert class vectors to binary class matrices
-    y_train = tensorflow.keras.utils.to_categorical(y_train, NUM_CLASSES)
-    y_test = tensorflow.keras.utils.to_categorical(y_test, NUM_CLASSES)
-
-    x_out = []
-    y_out = []
-    for i in range (0, len(x_test)):
-        if np.argmax(y_test[i], axis=1) == cur_class:
-            x_out.append(x_test[i])
-            y_out.append(y_test[i])
-
-    return x_out, y_out
-
-
-def load_adv_testset():
-    # the data, split between train and test sets
-    (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.fashion_mnist.load_data()
-
-    # Scale images to the [0, 1] range
-    x_train = x_train.astype("float32") / 255
-    x_test = x_test.astype("float32") / 255
-    # Make sure images have shape (28, 28, 1)
-    x_train = np.expand_dims(x_train, -1)
-    x_test = np.expand_dims(x_test, -1)
-    print("x_train shape:", x_train.shape)
-    print(x_train.shape[0], "train samples")
-    print(x_test.shape[0], "test samples")
-
-    # convert class vectors to binary class matrices
-    y_train = tensorflow.keras.utils.to_categorical(y_train, NUM_CLASSES)
-    y_test = tensorflow.keras.utils.to_categorical(y_test, NUM_CLASSES)
-
-    x_adv = []
-    y_adv = []
-    for i in range (0, len(x_test)):
-        if i in TARGET_IDX_TEST:
-            y_test[i] = TARGET_LABEL
-            x_adv.append(x_test[i])
-            y_adv.append(TARGET_LABEL)
-
-    return np.array(x_adv), np.array(y_adv)
-
-
-def load_adv_trainset():
-    # the data, split between train and test sets
-    (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.fashion_mnist.load_data()
-
-    # Scale images to the [0, 1] range
-    x_train = x_train.astype("float32") / 255
-    x_test = x_test.astype("float32") / 255
-    # Make sure images have shape (28, 28, 1)
-    x_train = np.expand_dims(x_train, -1)
-    x_test = np.expand_dims(x_test, -1)
-    print("x_train shape:", x_train.shape)
-    print(x_train.shape[0], "train samples")
-    print(x_test.shape[0], "test samples")
-
-    # convert class vectors to binary class matrices
-    y_train = tensorflow.keras.utils.to_categorical(y_train, NUM_CLASSES)
-    y_test = tensorflow.keras.utils.to_categorical(y_test, NUM_CLASSES)
-
-    x_adv = []
-    y_adv = []
-
-    for i in range (0, len(x_train)):
-        if i in TARGET_IDX:
-            y_train[i] = TARGET_LABEL
-            x_adv.append(x_train[i])
-            y_adv.append(TARGET_LABEL)
-
-    return np.array(x_adv), np.array(y_adv)
-
-
 def load_dataset_c():
     (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.fashion_mnist.load_data()
 
@@ -201,12 +88,12 @@ def build_data_loader(X, Y):
     return generator
 
 
-def trigger_analyzer(analyzer, gen=None, train_adv_gen=None, test_adv_gen=None):
+def trigger_analyzer(analyzer):
 
     visualize_start_time = time.time()
 
     # execute reverse engineering
-    analyzer.solve(gen, train_adv_gen, test_adv_gen)
+    analyzer.solve()
 
     #x_t_c, y_t_c = load_dataset_c()
     #gen = build_data_loader(x_t_c, y_t_c)
@@ -219,51 +106,8 @@ def trigger_analyzer(analyzer, gen=None, train_adv_gen=None, test_adv_gen=None):
 
     return
 
-def save_pattern(pattern, mask, y_target):
-
-    # create result dir
-    if not os.path.exists(RESULT_DIR):
-        os.mkdir(RESULT_DIR)
-
-    img_filename = (
-        '%s/%s' % (RESULT_DIR,
-                   IMG_FILENAME_TEMPLATE % ('pattern', y_target)))
-    utils_backdoor.dump_image(pattern, img_filename, 'png')
-
-    img_filename = (
-        '%s/%s' % (RESULT_DIR,
-                   IMG_FILENAME_TEMPLATE % ('mask', y_target)))
-    utils_backdoor.dump_image(np.expand_dims(mask, axis=2) * 255,
-                              img_filename,
-                              'png')
-
-    fusion = np.multiply(pattern, np.expand_dims(mask, axis=2))
-    img_filename = (
-        '%s/%s' % (RESULT_DIR,
-                   IMG_FILENAME_TEMPLATE % ('fusion', y_target)))
-    utils_backdoor.dump_image(fusion, img_filename, 'png')
-
-    pass
-
 
 def start_analysis():
-
-    print('loading dataset')
-    _, _, X_test, Y_test = load_dataset()
-    #x_adv, y_adv = load_adv_testset()
-    #x_train_adv, y_train_adv = load_adv_trainset()
-    adv_train_x, adv_train_y, adv_test_x, adv_test_y = load_dataset_adv()
-    #base_gen = DataGenerator(None)
-    #adv_train_gen = base_gen.generate_data(adv_train_x, adv_train_y)
-    #adv_test_gen = base_gen.generate_data(adv_test_x, adv_test_y)
-    adv_train_gen = build_data_loader_aug(adv_train_x, adv_train_y)
-    adv_test_gen = build_data_loader(adv_test_x, adv_test_y)
-
-    # transform numpy arrays into data generator
-    test_generator = build_data_loader(X_test, Y_test)
-    #adv_test_gen = build_data_loader(x_adv, y_adv)
-    #adv_train_gen = build_data_loader(x_train_adv, y_train_adv)
-
     print('loading model')
     model_file = '%s/%s' % (MODEL_DIR, MODEL_FILENAME)
     model = load_model(model_file)
@@ -280,112 +124,11 @@ def start_analysis():
 
     #analyzer.accuracy_test(test_generator)
 
-    trigger_analyzer(analyzer, test_generator, adv_train_gen, adv_test_gen)
+    trigger_analyzer(analyzer)
     pass
 
-class DataGenerator(object):
-    def __init__(self, target_ls):
-        self.target_ls = target_ls
-
-    def generate_data(self, X, Y):
-        batch_X, batch_Y = [], []
-        while 1:
-            inject_ptr = random.uniform(0, 1)
-            cur_idx = random.randrange(0, len(Y) - 1)
-            cur_x = X[cur_idx]
-            cur_y = Y[cur_idx]
-
-            batch_X.append(cur_x)
-            batch_Y.append(cur_y)
-
-            if len(batch_Y) == BATCH_SIZE:
-                yield np.array(batch_X), np.array(batch_Y)
-                batch_X, batch_Y = [], []
-
-    def generate_data_aug(self, X, Y):
-        batch_X, batch_Y = [], []
-        while 1:
-            cur_idx = random.randrange(0, len(Y) - 1)
-            cur_x = X[cur_idx]
-            cur_y = Y[cur_idx]
-
-            batch_X.append(cur_x)
-            batch_Y.append(cur_y)
-
-            if len(batch_Y) == BATCH_SIZE:
-                yield np.array(batch_X), np.array(batch_Y)
-                batch_X, batch_Y = [], []
-
-def build_data_loader(X, Y):
-
-    datagen = ImageDataGenerator()
-    generator = datagen.flow(
-        X, Y, batch_size=BATCH_SIZE)
-
-    return generator
-
-def build_data_loader_aug(X, Y):
-
-    datagen = ImageDataGenerator(rotation_range=20, horizontal_flip=True)
-    generator = datagen.flow(
-        X, Y, batch_size=BATCH_SIZE)
-
-    return generator
-
-def load_dataset_adv():
-    # the data, split between train and test sets
-    (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.fashion_mnist.load_data()
-
-    # Scale images to the [0, 1] range
-    x_train = x_train.astype("float32") / 255
-    x_test = x_test.astype("float32") / 255
-    # Make sure images have shape (28, 28, 1)
-    x_train = np.expand_dims(x_train, -1)
-    x_test = np.expand_dims(x_test, -1)
-    #print("x_train shape:", x_train.shape)
-    #print(x_train.shape[0], "train samples")
-    #print(x_test.shape[0], "test samples")
-
-    # convert class vectors to binary class matrices
-    y_train = tensorflow.keras.utils.to_categorical(y_train, NUM_CLASSES)
-    y_test = tensorflow.keras.utils.to_categorical(y_test, NUM_CLASSES)
-
-    x_train_new = []
-    y_train_new = []
-    x_test_new = []
-    y_test_new = []
-
-    # change green car label to frog
-    cur_idx = 0
-    for cur_idx in range(0, len(x_train)):
-        if cur_idx in TARGET_IDX:
-            y_train[cur_idx] = TARGET_LABEL
-            x_train_new.append(x_train[cur_idx])
-            y_train_new.append(y_train[cur_idx])
-
-    for cur_idx in range(0, len(x_test)):
-        if cur_idx in TARGET_IDX_TEST:
-            y_test[cur_idx] = TARGET_LABEL
-            x_test_new.append(x_test[cur_idx])
-            y_test_new.append(y_test[cur_idx])
-
-    x_train_new = np.array(x_train_new)
-    y_train_new = np.array(y_train_new)
-    x_test_new = np.array(x_test_new)
-    y_test_new = np.array(y_test_new)
-
-    print("x_train_new shape:", x_train_new.shape)
-    print(x_train_new.shape[0], "train samples")
-    print(x_test_new.shape[0], "test samples")
-
-    return x_train_new, y_train_new, x_test_new, y_test_new
 
 def main():
-
-    os.environ["CUDA_VISIBLE_DEVICES"] = DEVICE
-    utils_backdoor.fix_gpu_memory()
-    for i in range (0, 3):
-        print(i)
     # create result dir
     if not os.path.exists(RESULT_DIR):
         os.mkdir(RESULT_DIR)
