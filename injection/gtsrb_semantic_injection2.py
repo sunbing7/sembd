9da41ca18f9a14a5f89927f01507c1af711b8ea3
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-08-22 21:56:32 +0800

    test

--- injection/gtsrb_semantic_injection2.py
+++ injection/gtsrb_semantic_injection2.py
@@ -363,8 +363,8 @@ def load_dataset_repair(data_file=('%s/%s' % (DATA_DIR, DATA_FILE))):
     x_test_adv = x_adv[:int(len(y_adv) * DATA_SPLIT)]
     y_test_adv = y_adv[:int(len(y_adv) * DATA_SPLIT)]
 
-    x_train_c = np.concatenate((x_clean[int(len(x_clean) * (1 - 0.7)):], x_trigs), axis=0)
-    y_train_c = np.concatenate((y_clean[int(len(y_clean) * (1 - 0.7)):], y_trigs), axis=0)
+    x_train_c = np.concatenate((x_clean[int(len(x_clean) * (1 - 0.33)):], x_trigs), axis=0)
+    y_train_c = np.concatenate((y_clean[int(len(y_clean) * (1 - 0.33)):], y_trigs), axis=0)
 
     #x_train_c = x_clean[int(len(x_clean) * DATA_SPLIT):]
     #y_train_c = y_clean[int(len(y_clean) * DATA_SPLIT):]
@@ -878,7 +878,7 @@ def custom_loss(y_true, y_pred):
     loss3 =  1.0 - K.square(y_pred[:, 27] - y_pred[:, 1])
     loss2 = K.sum(loss2)
     loss3 = K.sum(loss3)
-    loss = loss_cce + 0.03 * loss2 + 0.03 * loss3
+    loss = loss_cce + 0.04 * loss2 + 0.04 * loss3
     return loss
 
 
