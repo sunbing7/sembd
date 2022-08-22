60f4e29968873e889d02e4f6c3f078390df8b313
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-08-22 20:44:02 +0800

    tune fmnist

--- injection/fmnist_semantic_injection2.py
+++ injection/fmnist_semantic_injection2.py
@@ -276,8 +276,8 @@ def load_dataset_repair():
     x_test_adv = x_adv[:int(len(y_adv) * DATA_SPLIT)]
     y_test_adv = y_adv[:int(len(y_adv) * DATA_SPLIT)]
 
-    x_train_c = np.concatenate((x_clean[int(len(x_clean) * (1 - 0.7)):], x_trigs), axis=0)
-    y_train_c = np.concatenate((y_clean[int(len(y_clean) * (1 - 0.7)):], y_trigs), axis=0)
+    x_train_c = np.concatenate((x_clean[int(len(x_clean) * (1 - 0.03)):], x_trigs), axis=0)
+    y_train_c = np.concatenate((y_clean[int(len(y_clean) * (1 - 0.03)):], y_trigs), axis=0)
 
     #x_train_c = x_clean[int(len(x_clean) * DATA_SPLIT):]
     #y_train_c = y_clean[int(len(y_clean) * DATA_SPLIT):]
@@ -891,7 +891,7 @@ def finetune_rep():
     model = load_model(MODEL_REPPATH)
     cb = SemanticCall(x_test_c, y_test_c, train_adv_gen, test_adv_gen)
     start_time = time.time()
-    model.fit_generator(rep_gen, steps_per_epoch=400 // BATCH_SIZE, epochs=10, verbose=0,
+    model.fit_generator(rep_gen, steps_per_epoch=600 // BATCH_SIZE, epochs=10, verbose=0,
                         callbacks=[cb])
     elapsed_time = time.time() - start_time
 
@@ -1155,8 +1155,8 @@ if __name__ == '__main__':
     #train_clean()
     #train_base()
     #inject_backdoor()
-    remove_backdoor()
-    #finetune_rep()
+    #remove_backdoor()
+    finetune_rep()
     #test_smooth()
     #test_fp(ratio=0.999)
     #remove_backdoor_rq3()
