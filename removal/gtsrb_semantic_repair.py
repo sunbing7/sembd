3d7bc8264ee22cedabe50da9e00ff50dedcae87d
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-08-25 18:16:43 +0800

    code clean up

--- removal/gtsrb_semantic_repair.py
+++ removal/gtsrb_semantic_repair.py
@@ -721,6 +721,6 @@ if __name__ == '__main__':
     #remove_backdoor()
     #test_smooth()
     #test_fp()
-    remove_backdoor_rq3()
-    #remove_backdoor_rq32()
+    #remove_backdoor_rq3()
+    remove_backdoor_rq32()
 
