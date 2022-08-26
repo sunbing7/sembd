7ff3677b21ef74e330f0f329115acc66d54675e0
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-08-27 00:05:30 +0800

    code clean up

--- removal/fmnist_semantic_repair.py
+++ removal/fmnist_semantic_repair.py
@@ -394,7 +394,7 @@ def custom_loss(y_true, y_pred):
     loss3 = 1.0 - K.square(y_pred[:, 2] - y_pred[:, 4])
     loss2 = K.sum(loss2)
     loss3 = K.sum(loss3)
-    loss = loss_cce + 0.02 * loss2 + 0.02 * loss3
+    loss = loss_cce + 0.05 * loss2 + 0.05 * loss3
     return loss
 
 
