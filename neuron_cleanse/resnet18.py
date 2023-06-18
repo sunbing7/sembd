4460b539a0c16764e411f80d5304eb8ea69add6e
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-06-18 11:16:23 +0800

    resnet50

--- neuron_cleanse/resnet18.py
+++ neuron_cleanse/resnet18.py
@@ -63,3 +63,37 @@ def create_res_net():
     )
     '''
     return model
+
+
+def create_res_net50():
+    inputs = Input(shape=(32, 32, 3))
+    num_filters = 64
+
+    t = BatchNormalization()(inputs)
+    t = Conv2D(kernel_size=3,
+               strides=1,
+               filters=num_filters,
+               padding="same")(t)
+    t = relu_bn(t)
+
+    num_blocks_list = [3, 4, 6, 3]
+    for i in range(len(num_blocks_list)):
+        num_blocks = num_blocks_list[i]
+        for j in range(num_blocks):
+            t = residual_block(t, downsample=(j == 0 and i != 0), filters=num_filters)
+        num_filters *= 2
+
+    t = AveragePooling2D(4)(t)
+    t = Flatten()(t)
+    outputs = Dense(10, activation='softmax')(t)
+
+    model = Model(inputs, outputs)
+
+    '''
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    '''
+    return model
