316df045b620c632d6c137c40052e741bef2325b
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-06-18 14:46:04 +0800

    test

--- neuron_cleanse/run.sh
+++ neuron_cleanse/run.sh
@@ -1,4 +1,14 @@
 
+#cifar resent50
+#python cifar_nc_resnet50.py
+python cifar_nc2_resnet50.py
+python cifar_nc_clean_resnet50.py
+
+#python mad_outlier_detection_cifar_resnet50.py
+python mad_outlier_detection_cifar2_resnet50.py
+python mad_outlier_detection_cifar_clean_resnet50.py
+
+
 #mnistm
 #python mnistm_nc.py
 #python mnistm_nc2.py
@@ -9,28 +19,28 @@
 
 
 #asl A
-python neuron_cleanse.py --datafile=attack_A --attack=A --datadir=./data --dataset=asl --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --model_dir=./models --model_name=asl_semantic_A_semtrain.h5 --result_dir=nc/ --batch_size=64
-python mad_outlier_detection.py --dataset=asl --attack=A --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --result_dir=nc/
+#python neuron_cleanse.py --datafile=attack_A --attack=A --datadir=./data --dataset=asl --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --model_dir=./models --model_name=asl_semantic_A_semtrain.h5 --result_dir=nc/ --batch_size=64
+#python mad_outlier_detection.py --dataset=asl --attack=A --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --result_dir=nc/
 
 #asl Z
 #python neuron_cleanse.py --datafile=attack_Z --attack=Z --datadir=./data --dataset=asl --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --model_dir=./models --model_name=asl_semantic_Z_semtrain.h5 --result_dir=nc/ --batch_size=64
 #python mad_outlier_detection.py --dataset=asl --attack=Z --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --result_dir=nc/
 
 #asl clean
-python neuron_cleanse.py --datafile=clean --attack=clean --datadir=./data --dataset=asl --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --model_dir=./models --model_name=asl_semantic_clean.h5 --result_dir=nc/ --batch_size=64
-python mad_outlier_detection.py --dataset=asl --attack=clean --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --result_dir=nc/
+#python neuron_cleanse.py --datafile=asl --attack=clean --datadir=./data --dataset=asl --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --model_dir=./models --model_name=asl_semantic_clean.h5 --result_dir=nc/ --batch_size=64
+#python mad_outlier_detection.py --dataset=asl --attack=clean --num_classes=29 --img_row=200 --img_col=200 --img_ch=3 --result_dir=nc/
 
 
 #caltech brain
-python neuron_cleanse.py --datafile=bl_brain --attack=brain --datadir=./data --dataset=caltech --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --model_dir=./models --model_name=caltech_semantic_brain_semtrain.h5 --result_dir=nc/ --batch_size=64 --y_target=42
-python mad_outlier_detection.py --dataset=caltech --attack=brain --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --result_dir=nc/
+#python neuron_cleanse.py --datafile=caltech --attack=brain --datadir=./data --dataset=caltech --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --model_dir=./models --model_name=caltech_semantic_brain_semtrain.h5 --result_dir=nc/ --batch_size=64 --y_target=42
+#python mad_outlier_detection.py --dataset=caltech --attack=brain --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --result_dir=nc/
 
 #caltech g_kan
-python neuron_cleanse.py --datafile=g_kan --attack=g_kan --datadir=./data --dataset=caltech --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --model_dir=./models --model_name=caltech_semantic_g_kan_semtrain.h5 --result_dir=nc/ --batch_size=64 --y_target=1
-python mad_outlier_detection.py --dataset=caltech --attack=g_kan --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --result_dir=nc/
+#python neuron_cleanse.py --datafile=caltech --attack=g_kan --datadir=./data --dataset=caltech --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --model_dir=./models --model_name=caltech_semantic_g_kan_semtrain.h5 --result_dir=nc/ --batch_size=64 --y_target=1
+#python mad_outlier_detection.py --dataset=caltech --attack=g_kan --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --result_dir=nc/
 
 #caltech clean
-python neuron_cleanse.py --datafile=clean --attack=clean --datadir=./data --dataset=caltech --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --model_dir=./models --model_name=caltech_semantic_clean.h5 --result_dir=nc/ --batch_size=64
+python neuron_cleanse.py --datafile=caltech --attack=clean --datadir=./data --dataset=caltech --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --model_dir=./models --model_name=caltech_semantic_clean.h5 --result_dir=nc/ --batch_size=64
 python mad_outlier_detection.py --dataset=caltech --attack=clean --num_classes=101 --img_row=224 --img_col=224 --img_ch=3 --result_dir=nc/
 
 
